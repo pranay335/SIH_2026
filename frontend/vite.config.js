16ccfe81982e6e03540c92dfed98037ed2c622ae
Virax17 <ntmainscinay@gmail.com>
2026-02-12 11:39:17 +0530

    Past changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< Updated upstream
+    port: 3000,
+    open: true,        // Auto-open browser
+    host: true,         // Expose to network
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -13,10 +15,5 @@ export default defineConfig({
         secure: false,
       }
     }
-=======
-    port: 3000,
-    open: true,        // Auto-open browser
-    host: true         // Expose to network
->>>>>>> Stashed changes
   }
 })
